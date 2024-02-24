54cca0fb792b1be5dde6386f69e9535e046f655e
Sami Adel <sami.adel.galal02@gmail.com>
2024-02-24 17:40:01 +0200

    a3333

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        API_URL: "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",
+    }
+};
 
 export default nextConfig;
